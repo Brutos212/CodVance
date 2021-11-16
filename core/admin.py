182a72b014b47bd013c917b74a2ef010fe83fe81
Brutos212 <fabio212@hotmail.com>
2021-11-16 12:23:31 -0300

    acertando alguns detalhes

--- core/admin.py
+++ core/admin.py
@@ -35,12 +35,3 @@ class ModelProvidorToProfileAdmin(admin.ModelAdmin):
 @admin.register(PaymentLog)
 class ModelPaymentLogAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'payment_id', 'status')
-
-    '''def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_add_permission(self, request):
-        return False'''
